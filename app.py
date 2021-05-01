db290df2b473314c0016d061d1cf025a36b13577
Tiago Pereira <targpereira@gmail.com>
2021-05-01 18:18:13 +0100

    OK

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-from bbquote.quote import get_quote
+from bbquote.lib import get_quote
 
 author, quote = get_quote()  # assuming the function returns an author and a quote
 
 f"{quote}, {author}"
+
